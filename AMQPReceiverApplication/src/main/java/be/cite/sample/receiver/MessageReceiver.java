adb86608882c533bfd5c39aba3b90bcda6c8ed0b
cedricclaus <cedric.claus@gmail.com>
2019-12-19 14:15:52 +0100

    Introduce Error in Receiver Treatment --> throw AmqpRejectAndDontRequeueException --> message is not resend to the Queue

--- AMQPReceiverApplication/src/main/java/be/cite/sample/receiver/MessageReceiver.java
+++ AMQPReceiverApplication/src/main/java/be/cite/sample/receiver/MessageReceiver.java
@@ -4,6 +4,7 @@ package be.cite.sample.receiver;
 import be.cite.sample.domain.MessageDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 
 import static be.cite.sample.config.RabbitMQConfiguration.AMQP_SAMPLES_MSG_QUEUE;
@@ -20,11 +21,11 @@ public class MessageReceiver {
     }
 
     @RabbitListener(queues = AMQP_SAMPLES_MSG_QUEUE)
-    public void receive(MessageDTO msg) throws Exception {
+    public void receive(MessageDTO msg)  {
         log.info("RECEIVED Message");
         log.info(msg.toString());
         if(!msg.getGood()){
-            throw new Exception("Could not handle message");
+            throw new AmqpRejectAndDontRequeueException("Could not handle message");
         }
     }
 
